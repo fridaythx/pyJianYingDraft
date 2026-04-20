9cb9af15fb86f050ce11ccb6cdbad23cef25d325
friday <friday.thx@gmail.com>
2026-04-20 10:05:59 +0800

    update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,12 +1,26 @@
 pid_file=.pyjianying.pid
 
 if [ -f ~/$pid_file ]; then
-  kill `cat ~/$pid_file`
-  echo 'kill' `cat ~/$pid_file`
+  ps -p `cat ~/$pid_file` > /dev/null 2>&1
+
+  rm ~/$pid_file
+
+  if [ $? -eq 0 ]; then
+    echo "Process is running with PID `cat ~/$pid_file`"
+
+    echo 'kill' `cat ~/$pid_file`
+    kill `cat ~/$pid_file`
+  else
+    echo "Process with PID `cat ~/$pid_file` is not running"
+  fi
 fi
 
 source .venv/bin/activate
 
 nohup python http_server.py > nohup.out 2>&1 &
+pid=$!
 
-echo $! | tee ~/$pid_file
+echo "Started process with PID $pid"
+echo $pid > ~/$pid_file
+
+
